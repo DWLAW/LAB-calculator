19f3c8f8941823f895fa4ad1cd81e3860d507b69
Dustin L <lawson.dustin12@gmail.com>
2024-04-18 20:07:26 -0500

    working buttons

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+// display function
 console.log("calculator");
+ function display(val){
+  document.getElementById('result').value += val
+  return val
+ }
+// solve function
+function solve(){
+  let x = document.getElementById('result').value;
+  let y = eval(x);
+  document.getElementById('result').value = y
+  return y
+}
+
+// clear function
+function clearScreen(){
+  document.getElementById('result').value = 0;
+}
+
+// backspace function
+
+  function backSpace(){
+    var value = document.getElementById("result").value;
+    document.getElementById("result").value = value.substr(0, value.length - 1);
+   
+  }
